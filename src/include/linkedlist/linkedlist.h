944c1af23e56694c8188590002643e674f6da081
Ciao <guza@duck.com>
2024-11-03 09:55:32 +0800

    feat: work on linkedlist; fix: specify cmake build type as Debug (#10)

---
+++ src/include/linkedlist/linkedlist.h
@@ -0,0 +1,243 @@
+#ifndef __LINKEDLIST_H__
+#define __LINKEDLIST_H__
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace linkedlist {
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int val) : val(val), next(nullptr) {}
+  ListNode(int val, ListNode *next) : val(val), next(next) {}
+};
+
+inline void freeLinkedList(linkedlist::ListNode *head) {
+  std::unordered_set<linkedlist::ListNode *> visited;
+  while (head != nullptr) {
+    if (visited.count(head)) {
+      break;
+    }
+    visited.insert(head);
+    auto next = head->next;
+    delete head;
+    head = next;
+  }
+}
+
+template <typename... Args>
+void freeLinkedLists(Args... args) {
+  (freeLinkedList(args), ...);
+}
+
+/// @brief given a linked list, determine if it has a cycle in it
+/// @param head the head of the linked list
+/// @return true if there is a cycle, false otherwise
+///
+/// constraints:
+/// - the number of the nodes in the list is in the range [0, 10^4]
+///
+/// - -10^5 <= Node.val <= 10^5
+///
+/// - pos is -1 or a valid index in the linked-list
+///
+/// @note https://leetcode.com/problems/linked-list-cycle/
+bool hasCycle(ListNode *head);
+
+/// @brief add two numbers represented by linked lists. The digits are stored in reverse order
+/// @param l1 the first linked list
+/// @param l2 the second linked list
+/// @return the sum of the two numbers
+///
+/// constraints:
+/// - the number of nodes in each linked list is in the range [1, 100]
+///
+/// - 0 <= Node.val <= 9
+///
+/// - it is guaranteed that the list represents a number that does not have leading zeros
+///
+/// @note https://leetcode.com/problems/add-two-numbers/
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2);
+
+/// @brief merge two sorted linked lists
+/// @param list1 the first linked list
+/// @param list2 the second linked list
+/// @return the merged linked list
+///
+/// constraints:
+///
+/// - the number of nodes in both lists is in the range [0, 50]
+///
+/// - -100 <= Node.val <= 100
+///
+/// - both list1 and list2 are sorted in non-decreasing order
+///
+/// @note https://leetcode.com/problems/merge-two-sorted-lists/
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2);
+
+class Node {
+ public:
+  int val;
+  Node *next;
+  Node *random;
+  Node(int _val) : val(_val), next(nullptr), random(nullptr) {}
+  Node(const Node &other) : val(other.val), next(other.next), random(other.random) {}
+};
+
+/// @brief deep-copy a linked list with random pointers
+/// @param head the head of the linked list
+/// @return the copied linked list
+///
+/// constraints:
+///
+/// - -10^4 <= Node.val <= 10^4
+///
+/// - 0 <= n <= 1000
+///
+/// @note https://leetcode.com/problems/copy-list-with-random-pointer/
+Node *copyRandomList(Node *head);
+
+/// @brief reverse a linked list between two positions
+/// @param head the head of the linked list
+/// @param left the left position
+/// @param right the right position
+/// @return the reversed linked list
+///
+/// constraints:
+///
+/// - 1 <= left <= right <= n
+///
+/// - 1 <= n <= 500
+///
+/// - -500 <= Node.val <= 500
+///
+/// @note https://leetcode.com/problems/reverse-linked-list-ii/
+ListNode *reverseBetween(ListNode *head, int left, int right);
+
+/// @brief reverse the linked list
+/// @param head the head of the linked list
+/// @return the reversed linked list
+///
+/// constraints:
+///
+/// - 0 <= n <= 5000
+///
+/// - -5000 <= Node.val <= 5000
+///
+/// @note https://leetcode.com/problems/reverse-linked-list/
+ListNode *reverseList(ListNode *head);
+
+/// @brief reverse the nodes of the list k at a time
+/// If the number of nodes is not a multiple of k then the nodes that are left out at the end should remain as is.
+/// @param head the head of the linked list
+/// @param k the number of nodes to reverse
+/// @return the reversed linked list
+///
+/// constraints:
+///
+/// - 1 <= k <= n <= 5000
+///
+/// - 0 <= Node.val <= 1000
+///
+/// @note https://leetcode.com/problems/reverse-nodes-in-k-group/
+ListNode *reverseKGroup(ListNode *head, int k);
+
+/// @brief remove the nth node from the end of the list
+/// @param head the head of the linked list
+/// @param n the nth node to remove
+/// @return the modified linked list
+///
+/// constraints:
+///
+/// - 1 <= sz <= 30
+///
+/// - 1 <= n <= sz
+///
+/// @note https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+ListNode *removeNthFromEnd(ListNode *head, int n);
+
+/// @brief remove duplicates from a sorted linked list
+/// @param head the head of the linked list
+/// @return the modified linked list
+///
+/// constraints:
+///
+/// - 0 <= the number of nodes <= 300
+///
+/// - -100 <= Node.val <= 100
+///
+/// @note https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ListNode *deleteDuplicates(ListNode *head);
+
+/// @brief rotate the list to the right by k places
+/// @param head the head of the linked list
+/// @param k the number of places to rotate
+/// @return the rotated linked list
+///
+/// constraints:
+///
+/// - -100 <= Node.val <= 100
+///
+/// - 1 <= k <= 2 * 10^9
+///
+/// @note https://leetcode.com/problems/rotate-list/
+ListNode *rotateRight(ListNode *head, int k);
+
+/// @brief partition the linked list such that all nodes less than x come before nodes greater than or equal to x
+/// @param head the head of the linked list
+/// @param x the partition value
+/// @return the partitioned linked list
+///
+/// constraints:
+///
+/// - 0 <= sz <= 200
+///
+/// - -100 <= Node.val <= 100
+///
+/// - -200 <= x <= 200
+///
+/// @note https://leetcode.com/problems/partition-list/
+ListNode *partition(ListNode *head, int x);
+
+/// @brief LRU cache
+///
+/// constraints:
+///
+/// - 1 <= capacity <= 3000
+///
+/// - 0 <= key <= 10^4
+///
+/// - 0 <= value <= 10^5
+///
+/// @note https://leetcode.com/problems/lru-cache/
+class LRUCache {
+ private:
+  struct CacheNode {
+    int key;
+    int value;
+    CacheNode *prev;
+    CacheNode *next;
+    CacheNode(int k, int v) : key(k), value(v), prev(nullptr), next(nullptr) {}
+  };
+
+  int capacity_;
+  std::unordered_map<int, CacheNode *> cache_;
+  CacheNode *head_;  // Most recently used
+  CacheNode *tail_;  // Least recently used
+
+  void moveToFront(CacheNode *node);
+  void addToFront(CacheNode *node);
+  void removeLRU();
+
+ public:
+  explicit LRUCache(int capacity);
+  ~LRUCache();
+
+  int get(int key);
+  void put(int key, int value);
+};
+
+}  // namespace linkedlist
+
+#endif  // __LINKEDLIST_H__
---
+++ src/linkedlist/linkedlist.cpp
@@ -0,0 +1,333 @@
+#include "linkedlist/linkedlist.h"
+
+#include <unordered_map>
+
+namespace linkedlist {
+
+bool hasCycle(ListNode *head) {
+  if (head == nullptr) {
+    return false;
+  }
+
+  auto slow = head;
+  auto fast = head->next;
+  while (slow != fast) {
+    if (fast == nullptr || fast->next == nullptr) {
+      return false;
+    }
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  return true;
+}
+
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+  ListNode dummy(0);
+  auto curr = &dummy;
+  int carry = 0;
+  while (l1 != nullptr || l2 != nullptr) {
+    int x = l1 != nullptr ? l1->val : 0;
+    int y = l2 != nullptr ? l2->val : 0;
+    int sum = x + y + carry;
+    carry = sum / 10;
+    curr->next = new ListNode(sum % 10);
+    curr = curr->next;
+
+    if (l1 != nullptr) {
+      l1 = l1->next;
+    }
+    if (l2 != nullptr) {
+      l2 = l2->next;
+    }
+  }
+
+  if (carry > 0) {
+    curr->next = new ListNode(carry);
+  }
+
+  return dummy.next;
+}
+
+ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+  ListNode dummy(0);
+  auto curr = &dummy;
+  while (list1 != nullptr && list2 != nullptr) {
+    if (list1->val < list2->val) {
+      curr->next = list1;
+      list1 = list1->next;
+    } else {
+      curr->next = list2;
+      list2 = list2->next;
+    }
+    curr = curr->next;
+  }
+  curr->next = list1 != nullptr ? list1 : list2;
+  return dummy.next;
+}
+
+Node *copyRandomList(Node *head) {
+  std::unordered_map<Node *, Node *> map;
+  Node dummy(0);
+  auto curr = &dummy;
+  while (head != nullptr) {
+    map[head] = new Node(*head);
+    curr->next = map[head];
+    curr = curr->next;
+    head = head->next;
+  }
+
+  head = dummy.next;
+  while (head != nullptr) {
+    head->random = map[head->random];
+    head = head->next;
+  }
+  return dummy.next;
+}
+
+ListNode *reverseBetween(ListNode *head, int left, int right) {
+  int pos = 0;
+  ListNode dummy(0, head);
+  auto prev = &dummy;
+  while (pos < left - 1) {
+    prev = prev->next;
+    ++pos;
+  }
+
+  auto curr = prev->next;
+  for (int i = 0; i < right - left; ++i) {
+    auto nxt = curr->next;
+    curr->next = nxt->next;
+    nxt->next = prev->next;
+    prev->next = nxt;
+  }
+
+  return dummy.next;
+}
+
+ListNode *reverseList(ListNode *head) {
+  ListNode *prev = nullptr;
+  while (head != nullptr) {
+    auto nxt = head->next;
+    head->next = prev;
+    prev = head;
+    head = nxt;
+  }
+  return prev;
+}
+
+ListNode *reverseKGroup(ListNode *head, int k) {
+  if (head == nullptr || k == 1) {
+    return head;
+  }
+
+  int cnt = 0;
+  auto curr = head;
+  while (curr != nullptr) {
+    curr = curr->next;
+    ++cnt;
+  }
+
+  ListNode dummy(0, head);
+  auto prev = &dummy;
+  curr = head;
+
+  while (cnt >= k) {
+    auto nextHead = curr;
+    for (int i = 0; i < k; i++) {
+      nextHead = nextHead->next;
+    }
+
+    ListNode *prevNode = nextHead;
+    auto currNode = curr;
+    for (int i = 0; i < k; i++) {
+      auto next = currNode->next;
+      currNode->next = prevNode;
+      prevNode = currNode;
+      currNode = next;
+    }
+
+    prev->next = prevNode;
+    prev = curr;
+    curr = nextHead;
+    cnt -= k;
+  }
+
+  return dummy.next;
+}
+
+ListNode *rotateRight(ListNode *head, int k) {
+  if (head == nullptr || k == 0) {
+    return head;
+  }
+
+  int n = 1;
+  auto last = head;
+  while (last->next != nullptr) {
+    last = last->next;
+    n++;
+  }
+
+  k = k % n;
+  if (k == 0) {
+    return head;
+  }
+
+  ListNode dummy(0, head);
+  auto curr = &dummy;
+  for (int i = 0; i < n - k; i++) {
+    curr = curr->next;
+  }
+
+  auto newHead = curr->next;
+  curr->next = nullptr;
+  last->next = head;
+  return newHead;
+}
+
+ListNode *removeNthFromEnd(ListNode *head, int n) {
+  ListNode dummy(0, head);
+  auto curr = head;
+  int cnt = 1;
+  while (curr->next != nullptr) {
+    curr = curr->next;
+    ++cnt;
+  }
+
+  auto prev = &dummy;
+  curr = head;
+  for (int i = 0; i < cnt - n; i++) {
+    prev = prev->next;
+    curr = curr->next;
+  }
+
+  prev->next = curr->next;
+  return dummy.next;
+}
+
+ListNode *deleteDuplicates(ListNode *head) {
+  if (head == nullptr) {
+    return nullptr;
+  }
+
+  ListNode dummy(0, head);
+  auto prev = &dummy;
+  auto curr = head;
+
+  while (curr != nullptr && curr->next != nullptr) {
+    if (int val = curr->val; val == curr->next->val) {
+      while (curr != nullptr && curr->val == val) {
+        curr = curr->next;
+      }
+      prev->next = curr;
+    } else {
+      prev = curr;
+      curr = curr->next;
+    }
+  }
+
+  return dummy.next;
+}
+
+ListNode *partition(ListNode *head, int x) {
+  ListNode leftDummy(0);
+  ListNode rightDummy(0);
+  auto left = &leftDummy;
+  auto right = &rightDummy;
+  while (head != nullptr) {
+    if (head->val < x) {
+      left->next = head;
+      left = left->next;
+    } else {
+      right->next = head;
+      right = right->next;
+    }
+    head = head->next;
+  }
+
+  left->next = rightDummy.next;
+  right->next = nullptr;
+  return leftDummy.next;
+}
+
+void LRUCache::moveToFront(CacheNode *node) {
+  if (node == head_) {
+    return;
+  }
+
+  // Remove from current position
+  node->prev->next = node->next;
+  if (node->next != nullptr) {
+    node->next->prev = node->prev;
+  } else {
+    tail_ = node->prev;
+  }
+
+  // Move to front
+  node->next = head_;
+  node->prev = nullptr;
+  head_->prev = node;
+  head_ = node;
+}
+
+void LRUCache::addToFront(CacheNode *node) {
+  if (head_ == nullptr) {
+    head_ = tail_ = node;
+    return;
+  }
+
+  node->next = head_;
+  head_->prev = node;
+  head_ = node;
+}
+
+void LRUCache::removeLRU() {
+  if (tail_ == nullptr) {
+    return;
+  }
+
+  cache_.erase(tail_->key);
+  CacheNode *temp = tail_;
+  tail_ = tail_->prev;
+  if (tail_ != nullptr) {
+    tail_->next = nullptr;
+  } else {
+    head_ = nullptr;
+  }
+  delete temp;
+}
+
+LRUCache::LRUCache(int capacity) : capacity_(capacity), head_(nullptr), tail_(nullptr) {}
+
+LRUCache::~LRUCache() {
+  while (head_) {
+    CacheNode *temp = head_;
+    head_ = head_->next;
+    delete temp;
+  }
+}
+
+int LRUCache::get(int key) {
+  if (auto it = cache_.find(key); it != cache_.end()) {
+    moveToFront(it->second);
+    return it->second->value;
+  }
+  return -1;
+}
+
+void LRUCache::put(int key, int value) {
+  if (auto it = cache_.find(key); it != cache_.end()) {
+    it->second->value = value;
+    moveToFront(it->second);
+    return;
+  }
+
+  auto *node = new CacheNode(key, value);
+  cache_[key] = node;
+  addToFront(node);
+
+  if (static_cast<int>(cache_.size()) > capacity_) {
+    removeLRU();
+  }
+}
+
+}  // namespace linkedlist
